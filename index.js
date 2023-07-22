1678b558a5001431450f1420b7cec2e83ab1044b
jakariachynabil10 <jakariachynabil@gmail.com>
2023-07-22 07:16:09 +0600

    all colleges added to the database and api is ready

--- index.js
+++ index.js
@@ -1,11 +1,59 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 const port = 4612
 
+
+app.use(cors())
+app.use(express.json())
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aoalbnp.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const allColleges = client.db("eCommerceSite").collection("allColleges")
+
+    app.get("/allColleges", async (req, res)=>{
+      const result = await allColleges.find().toArray()
+      res.send(result)
+    })
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('eCommerce server is running')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`eCommerce server is running on port ${port}`)
 })
